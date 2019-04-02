437bb1886544780686cf3561f799b49e5308ea17
volocuga <tknoleg@gmail.com>
2019-04-02 22:36:59 +0300

    workspace settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    dudu: 'nn',
+  };
+
   render() {
+    const { dudu } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={dudu} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
